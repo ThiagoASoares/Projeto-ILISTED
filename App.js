13c883a26938ce9215f40a41b488e73392bdb3f2
Thiago A Soares <thiagoafonsosoares@gmail.com>
2022-03-31 21:03:06 -0300

    Tela de login

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Alert } from 'react-native';
 
 export default function App() {
+
+    clicou = () => {
+        Alert.alert("IListed","temos que programar este botao kkk")
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+          <Image
+              source={require('./assets/IListed.jpg')}
+              style={styles.logo}
+          />
+          <TextInput 
+              placeholder='Digite seu E_mail'
+              style={styles.input}
+          />
+          <TextInput
+              placeholder='Digite sua Senha'
+              secureTextEntry={true}
+              style={styles.input}
+          />
+          <TouchableOpacity
+              style={styles.botao}
+              onPress={() => { this.clicou() }}
+
+          >
+              <Text style={styles.botaoTxt}>Login</Text>
+          </TouchableOpacity>
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#458',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    logo: {
+        width: 140,
+        height: 130,
+        borderRadius: 100
+    },
+    input: {
+        marginTop: 20,
+        padding: 10,
+        width: 300,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    botao: {
+        width: 300,
+        height: 42,
+        backgroundColor:'#000000',
+        marginTop: 20,
+        borderRadius: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+},
+
+    botaoTxt:{
+        fontSize: 16,
+        fontWeight: 'bold',
+        color:'#fff',
+}
 });
